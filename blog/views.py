2ea6227d17bc8ce0430eda17fac0b9e1e3785453
jst1to5y <dev.tokhy@gmail.com>
2023-07-13 20:24:44 +0300

    create single post page

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,8 @@ def all_posts(request):
     data = Post.objects.all()
     
     return render(request, 'all_posts.html', {'posts': data})
+
+
+def single_post(request, post_id):
+    data = Post.objects.get(id= post_id)
+    return render(request, 'single_post.html', {'post': data})
--- project/urls.py
+++ project/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from blog.views import all_posts
+from blog.views import all_posts, single_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', all_posts)
+    path('blog/', all_posts),
+    path('blog/<int:post_id>/', single_post),
 ]
 
 
